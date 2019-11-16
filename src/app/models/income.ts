a6f7bd55689bf4e82ead4947e5208351445b1c7e
Vorrawut Judasri <vorrawutjud@gmail.com>
2019-11-16 12:04:58 +0700

    binding income

---
+++ src/app/models/income.ts
@@ -0,0 +1,7 @@
+export interface Income {
+  id: number;
+  incomeGroupId: number;
+  incomeNameGroupId: number;
+  amount: number;
+  date: string;
+}
--- src/app/pages/income/income.component.ts
+++ src/app/pages/income/income.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { Income } from 'src/app/models/income';
 
 @Component({
   selector: 'app-income',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./income.component.scss']
 })
 export class IncomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private modalService: BsModalService) {}
+  modalRef: BsModalRef;
+  income: Income;
 
   ngOnInit() {
+    this.income = {
+      id: 1,
+      incomeGroupId: 1,
+      incomeNameGroupId: 1,
+      amount: 10000,
+      date: '1/31/2019'
+    };
   }
 
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 }
