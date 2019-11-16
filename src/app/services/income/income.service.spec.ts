fcd673945c7cd95642adedd62c4418309cca665c
Vorrawut Judasri <vorrawutjud@gmail.com>
2019-11-16 15:05:42 +0700

    add get income service

--- src/app/services/income/income.service.spec.ts
+++ src/app/services/income/income.service.spec.ts
@@ -1,17 +1,32 @@
 import { TestBed } from '@angular/core/testing';
-
 import { IncomeService } from './income.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
 describe('IncomeService', () => {
   let service: IncomeService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(IncomeService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
+  it('should call method get with url of get income api', () => {
+    service.getIncomeByUserId().subscribe();
+
+    const req = httpTestingController.expectOne(
+      'http://103.74.254.157:9003/income/id/1'
+    );
+    expect(req.request.method).toBe('GET');
+  });
 });
--- src/app/services/income/income.service.ts
+++ src/app/services/income/income.service.ts
@@ -1,11 +1,15 @@
+import { Income } from 'src/app/models/income';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class IncomeService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
+  getIncomeByUserId(): Observable<Income[]> {
+    return this.http.get<Income[]>('http://103.74.254.157:9003/income/id/1');
+  }
 }
