6e0791278846e669c26a07c23232580b6ad7ed65
Vorrawut Judasri <vorrawutjud@gmail.com>
2019-11-16 11:34:21 +0700

    add  lib ngx boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IncomeComponent } from './pages/income/income.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { IncomeComponent } from './pages/income/income.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
