7f7582607a693f7d75d40273c04439bcf3d944e2
Vorrawut Judasri <vorrawutjud@gmail.com>
2019-11-16 13:30:13 +0700

    binging data income

--- src/app/pages/income/income.component.ts
+++ src/app/pages/income/income.component.ts
@@ -10,16 +10,25 @@ import { Income } from 'src/app/models/income';
 export class IncomeComponent implements OnInit {
   constructor(private modalService: BsModalService) {}
   modalRef: BsModalRef;
-  income: Income;
+  incomes: Income[];
 
   ngOnInit() {
-    this.income = {
-      id: 1,
-      incomeGroupId: 1,
-      incomeNameGroupId: 1,
-      amount: 10000,
-      date: '1/31/2019'
-    };
+    this.incomes = [
+      {
+        id: 1,
+        incomeGroupId: 1,
+        incomeNameGroupId: 1,
+        amount: 10000,
+        date: '1/31/2019'
+      },
+      {
+        id: 2,
+        incomeGroupId: 1,
+        incomeNameGroupId: 1,
+        amount: 500000,
+        date: '2/31/2019'
+      }
+    ];
   }
 
   openModal(template: TemplateRef<any>) {
